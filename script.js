e24db985c45519b5cd4d58ff22610a747c24bb50
Sam <imramsamin24@gmail.com>
2026-09-08 19:00:16 +0600

    email change final

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
    ========================================================= */
 
 const SUBMISSION_ENDPOINT =
-    "https://script.google.com/macros/s/AKfycbxkqvVpVGQ6N5M5sNl2QgOtjDKoesTKHsKNqJEavQyZQewU8eW0A0_Kb7Sf5TJBTmF6/exec";
+    "https://script.google.com/macros/s/AKfycbyG5fMWYIfAf5ze1-svpFw4_P6q6qRTLIOdzZGmkTNF5o5N9d8_guDNyvdpzf4MIiHZ/exec";
     
 /* =========================================================
    2. GET FORM ELEMENTS
